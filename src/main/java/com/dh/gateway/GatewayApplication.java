08df7bd4e6a76051c23105d932b8cd4303e4fb95
lee-dohyun <leedohyun1985@gmail.com>
2025-06-23 15:39:33 +0900

    수정 후 빌드 테스트

--- src/main/java/com/dh/gateway/GatewayApplication.java
+++ src/main/java/com/dh/gateway/GatewayApplication.java
@@ -2,12 +2,18 @@ package com.dh.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class GatewayApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(GatewayApplication.class);
+
 	public static void main(String[] args) {
+		logger.info("GatewayApplication 시작됨");
 		SpringApplication.run(GatewayApplication.class, args);
+		logger.info("SpringApplication.run() 호출 완료");
 	}
 
 }
